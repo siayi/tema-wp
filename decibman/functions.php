a5ff76023f3f11da136071469e5ed80ae8678710
siayi <ayisumarna@gmail.com>
2016-10-12 11:04:20 +0700

    update again

--- decibman/functions.php
+++ decibman/functions.php
@@ -121,3 +121,20 @@ function my_404_content(){
     );
 }
 //END 
+//START GOOGLE ANALYTICS TRACKING CODE
+add_action('wp_head','my_analytics', 20);
+function my_analytics() {
+?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42289839-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<?php
+}
+//END GOOGLE ANALYTICS TRACKING CODE
